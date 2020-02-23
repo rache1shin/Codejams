1195f8d703a924669cb5963f3d6636f00db59916
rache1shin <smals24.jm@gmail.com>
2020-02-23 22:24:45 +0900

    Solution passed

---
+++ SavingTheUniverseAgain/src/Solution.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.		
+		for (int i = 1; i <= t; ++i) {
+			int limit = in.nextInt();
+			String attack = in.next();
+			int num = runProgram(limit, attack);
+			if(num==-1) {
+				System.out.println("Case #" + i + ": IMPOSSIBLE");
+			}
+			else {
+				System.out.println("Case #"+i+" : "+num);
+			}
+		}
+	}
+	private static int runProgram(int limit, String attack) {
+		int[] arr = new int[20];
+		int pointer = 0;
+		int realPointer=0;
+		int change = 0;
+		for(int i=0; i<attack.length(); i++) {
+			if(attack.charAt(i) == 'S') {arr[pointer] += 1; realPointer=pointer;}
+			else {pointer++;}
+		}
+		pointer = realPointer;
+		while(pointer>0) {
+			int sum = getSum(arr, pointer);
+			if(sum<=limit) {
+				return change;
+			}
+			else if(sum>limit) {
+				if(arr[pointer] > 1) {
+					arr[pointer]--;
+					arr[pointer-1]++;
+				}
+				else if(arr[pointer] <= 1) {
+					pointer--;
+					arr[pointer]++;
+				}
+				change++;
+			}
+		}
+		if(getSum(arr,0)>limit)
+			return -1;
+		return change;
+	}
+	private static int getSum(int[] arr, int pointer) {
+		int sum = 0;
+		for(int i=0; i<=pointer; i++) {
+			sum += arr[i] * Math.pow(2, i);
+		}
+		return sum;
+	}
+	
+} 
